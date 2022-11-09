231acadb273cbc8edacad4b24ad26feb4b1e2324
mafe-a5 <20190930@aloe.ulima.edu.pe>
2022-11-08 21:06:29 -0500

    .

--- enunciado-ev03-2022-2/backend/app.js
+++ enunciado-ev03-2022-2/backend/app.js
@@ -33,6 +33,68 @@ app.get('/marca', (req,res,next)=> {
     return res.json(marcaData);
 })
 
+const modeloData = [
+    { id: 1, name: 'ID', 
+    programs: [
+        {id:1, name: '1'},
+        {id:2, name: '2'},
+        {id:3, name: '3'},
+        {id:4, name: '4'},
+        {id:5, name: '5'},
+        {id:6, name: '6'},
+        {id:7, name: '7'},
+        {id:8, name: '8'}
+    ]},
+    { id: 2, name: 'Nombre',
+    programs: [
+        {id:1, name: 'Tiida'},
+        {id:2, name: 'Sentra'},
+        {id:3, name: 'Note'},
+        {id:4, name: 'Camaro RS'},
+        {id:5, name: 'Cruze'},
+        {id:6, name: 'i3'},
+        {id:7, name: 'i4'},
+        {id:8, name: 'z4'}
+    ]},
+    { id: 3, name: 'Año',
+    programs: [
+        {id:1, name: '2022'},
+        {id:2, name: '2022'},
+        {id:3, name: '2020'},
+        {id:4, name: '2015'},
+        {id:5, name: '2022'},
+        {id:6, name: '2021'},
+        {id:7, name: '2021'},
+        {id:8, name: '2022'}
+    ]},
+    { id: 4, name: 'Código de marca',
+    programs: [
+        {id:1, name: '1'},
+        {id:2, name: '1'},
+        {id:3, name: '1'},
+        {id:4, name: '2'},
+        {id:5, name: '2'},
+        {id:6, name: '3'},
+        {id:7, name: '3'},
+        {id:8, name: '3'}
+    ]},
+    { id: 5, name: 'Nombre del país de origen',
+    programs: [
+        {id:1, name: 'Japón'},
+        {id:2, name: 'Japón'},
+        {id:3, name: 'Japón'},
+        {id:4, name: 'Estados Unidos'},
+        {id:5, name: 'Estados Unidos'},
+        {id:6, name: 'Alemania'},
+        {id:7, name: 'Alemania'},
+        {id:8, name: 'Alemania'}
+    ]}
+]
+
+app.get('/modelo', (req,res,next)=> {
+    return res.json(modeloData);
+})
+
 app.get('/',(req, res, next)=> {
     return res.json({ response: 'OK'});
 })
